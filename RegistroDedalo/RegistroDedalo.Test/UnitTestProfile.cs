1e7c31d1e2cbf16955adf8adc335ab540df3f1db
Francesco Tonini <francescoantoniotonini@gmail.com>
2015-11-17 06:04:48 +0100

    Test /auth/me

--- RegistroDedalo/RegistroDedalo.Test/UnitTestProfile.cs
+++ RegistroDedalo/RegistroDedalo.Test/UnitTestProfile.cs
@@ -27,13 +27,24 @@ namespace RegistroDedalo.Test
     [TestClass]
     public class UnitTestProfile
     {
-        // auth token for test profile
-        private string authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTAwLCJ1IjoidGVzdCIsImp0aSI6MSwiZGlkIjoiMTQ0NzUzNTU0OTgwMDExNSIsImlhdCI6MTQ0NzUzNTU1MSwiZXhwIjoxNDQ4ODMxNTUxfQ.lA8kOcnqRTD2YdPlLHBJ_0sYEZzm_oi4HxsxQADl-AU";
+        /// <summary>
+        /// Return an auth token of a test profile
+        /// </summary>
+        /// <returns>Auth token</returns>
+        private async Task<string> GetTestToken()
+        {
+            DedaloClient client = new DedaloClient();
+            DedaloResponse<Utente> response = await client.SignIn("test", "ciao");
+
+            return response.Data.AuthToken ?? null;
+        }
 
         [TestMethod]
         public async Task TestMeWithInternalAuthToken()
         {
-            DedaloClient client = new DedaloClient(this.authToken);
+            string authToken = await GetTestToken();
+        
+            DedaloClient client = new DedaloClient(authToken);
             DedaloResponse<Utente> response = await client.GetMe();
 
             Assert.IsNotNull(response.Data);
