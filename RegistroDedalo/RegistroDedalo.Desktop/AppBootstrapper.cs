336f0ee3834ed5fea04b8ef2058dfcdc67b3ae4c
Francesco Tonini <francescoantoniotonini@gmail.com>
2015-11-22 14:41:59 +0100

    Inizio sviluppo app desktop
    Aggiunta Caliburn Micro

---
+++ RegistroDedalo/RegistroDedalo.Desktop/AppBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using RegistroDedalo.Desktop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.Windows;
+
+namespace RegistroDedalo.Desktop
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            base.Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            base.DisplayRootViewFor<LoginViewModel>();
+        }
+    }
+}
