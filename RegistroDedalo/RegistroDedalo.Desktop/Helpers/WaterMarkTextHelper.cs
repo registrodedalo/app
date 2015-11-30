21ea32bd449951ef83a58ed6139558461f8e357e
Francesco Tonini <francescoantoniotonini@gmail.com>
2015-11-30 12:12:05 +0100

    Stile loginview

---
+++ RegistroDedalo/RegistroDedalo.Desktop/Helpers/WaterMarkTextHelper.cs
@@ -0,0 +1,135 @@
+﻿/*
+    Copyright (C) 2015 Dedalo
+    
+    This file is part of Dedalo.
+
+    Dedalo is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Dedalo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with Dedalo. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RegistroDedalo.Desktop.Helpers
+{
+    public class WaterMarkTextHelper : DependencyObject
+    {
+        #region Attached Properties
+
+        public static bool GetIsMonitoring(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsMonitoringProperty);
+        }
+
+        public static void SetIsMonitoring(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsMonitoringProperty, value);
+        }
+
+        public static readonly DependencyProperty IsMonitoringProperty =
+            DependencyProperty.RegisterAttached("IsMonitoring", typeof(bool), typeof(WaterMarkTextHelper), new UIPropertyMetadata(false, OnIsMonitoringChanged));
+
+
+        public static string GetWatermarkText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(WatermarkTextProperty);
+        }
+
+        public static void SetWatermarkText(DependencyObject obj, string value)
+        {
+            obj.SetValue(WatermarkTextProperty, value);
+        }
+
+        public static readonly DependencyProperty WatermarkTextProperty =
+            DependencyProperty.RegisterAttached("WatermarkText", typeof(string), typeof(WaterMarkTextHelper), new UIPropertyMetadata(string.Empty));
+
+
+        public static int GetTextLength(DependencyObject obj)
+        {
+            return (int)obj.GetValue(TextLengthProperty);
+        }
+
+        public static void SetTextLength(DependencyObject obj, int value)
+        {
+            obj.SetValue(TextLengthProperty, value);
+
+            if (value >= 1)
+                obj.SetValue(HasTextProperty, true);
+            else
+                obj.SetValue(HasTextProperty, false);
+        }
+
+        public static readonly DependencyProperty TextLengthProperty =
+            DependencyProperty.RegisterAttached("TextLength", typeof(int), typeof(WaterMarkTextHelper), new UIPropertyMetadata(0));
+
+        #endregion
+
+        #region Internal DependencyProperty
+
+        public bool HasText
+        {
+            get { return (bool)GetValue(HasTextProperty); }
+            set { SetValue(HasTextProperty, value); }
+        }
+
+        private static readonly DependencyProperty HasTextProperty =
+            DependencyProperty.RegisterAttached("HasText", typeof(bool), typeof(WaterMarkTextHelper), new FrameworkPropertyMetadata(false));
+
+        #endregion
+
+        #region Implementation
+
+        static void OnIsMonitoringChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox)
+            {
+                TextBox txtBox = d as TextBox;
+
+                if ((bool)e.NewValue)
+                    txtBox.TextChanged += TextChanged;
+                else
+                    txtBox.TextChanged -= TextChanged;
+            }
+            else if (d is PasswordBox)
+            {
+                PasswordBox passBox = d as PasswordBox;
+
+                if ((bool)e.NewValue)
+                    passBox.PasswordChanged += PasswordChanged;
+                else
+                    passBox.PasswordChanged -= PasswordChanged;
+            }
+        }
+
+        static void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            if (txtBox == null) return;
+            SetTextLength(txtBox, txtBox.Text.Length);
+        }
+
+        static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passBox = sender as PasswordBox;
+            if (passBox == null) return;
+            SetTextLength(passBox, passBox.Password.Length);
+        }
+
+        #endregion
+    }
+}
--- RegistroDedalo/RegistroDedalo.Desktop/Properties/Resources.Designer.cs
+++ RegistroDedalo/RegistroDedalo.Desktop/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RegistroDedalo.Desktop.Properties
-{
-
-
+namespace RegistroDedalo.Desktop.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,86 @@ namespace RegistroDedalo.Desktop.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    public class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        public static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RegistroDedalo.Desktop.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        public static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Registro elettronico digitale.
+        /// </summary>
+        public static string AppDescription {
+            get {
+                return ResourceManager.GetString("AppDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Dedalo.
+        /// </summary>
+        public static string AppName {
+            get {
+                return ResourceManager.GetString("AppName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Login.
+        /// </summary>
+        public static string LoginViewTitle {
+            get {
+                return ResourceManager.GetString("LoginViewTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Password.
+        /// </summary>
+        public static string PasswordLabel {
+            get {
+                return ResourceManager.GetString("PasswordLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Nome utente.
+        /// </summary>
+        public static string UsernameLabel {
+            get {
+                return ResourceManager.GetString("UsernameLabel", resourceCulture);
             }
         }
     }
